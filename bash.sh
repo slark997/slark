65153b4741b31d36ff50b5521f6559ba768a281f
root <root@slark.>
2024-10-06 14:35:33 +0000

    update bash

--- bash.sh
+++ bash.sh
@@ -1,2 +1,25 @@
-ls -la
-cat document.md
+#!/bin/bash
+
+CONTAINER_NAME="hospital-sql"
+MYSQL_ROOT_PASSWORD="my-secret-pw"
+MYSQL_DATABASE="mydb"
+MYSQL_USER="myuser"
+MYSQL_PASSWORD="mypassword"
+CONFIG_PATH=".my.cnf"
+
+cat << EOF | sudo tee -a ${CONFIG_PATH}
+[client]
+user=${MYSQL_USER}
+password=${MYSQL_PASSWORD}
+host=localhost
+EOF
+
+docker run -d \
+           --name $CONTAINER_NAME \
+           -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD \
+           -e MYSQL_DATABASE=$MYSQL_DATABASE \
+           -e MYSQL_USER=$MYSQL_USER \
+           -e MYSQL_PASSWORD=$MYSQL_PASSWORD \
+           -p 3306:3306 -d mysql:latest
+
+docker cp ${CONFIG_PATH} ${CONTAINER_NAME}:/root/${CONFIG_PATH}
